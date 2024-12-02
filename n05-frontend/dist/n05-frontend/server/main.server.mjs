ff2eb7d04f50c2b44c69e5614ccd7eaea1bde900
nekesuresh <88456215+nekesuresh@users.noreply.github.com>
2024-12-01 23:03:53 -0500

    Add Procfile for Heroku deployment

---
+++ n05-frontend/dist/n05-frontend/server/main.server.mjs
@@ -0,0 +1,10 @@
+import './polyfills.server.mjs';
+import {
+  main_server_default
+} from "./chunk-ITZAZKXQ.mjs";
+import "./chunk-SV6VKEKN.mjs";
+import "./chunk-PTRYWQQD.mjs";
+export {
+  main_server_default as default
+};
+//# sourceMappingURL=main.server.mjs.map
