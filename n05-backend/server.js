d7fb3367b301be4f608dfdb335de9b94f8a98edf
Neketha Suresh <88456215+nekesuresh@users.noreply.github.com>
2024-12-02 17:45:12 -0500

    Update server.js

--- n05-backend/server.js
+++ n05-backend/server.js
@@ -30,5 +30,6 @@ app.use(bodyParser.json());
 app.use('/api/charts', chartRoutes); 
 app.use('/api/auth', authRoutes); 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
